6850b3277acaf412a31cb8cca87237504596c715
JerecDev <zachabbett@gmail.com>
2021-11-10 15:42:46 -0800

    First Upload
    Mostly incomplete, has main menu without assets and several basic features

---
+++ JAPN350 Final/Assets/Scripts/BatleStart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatleStart : MonoBehaviour
+{
+
+    float m_distanceTraveled = 0f;
+    public GameObject groundImage;
+    public GameObject ohnishiSprite;
+    bool colorFlip;
+
+    void Start()
+    {
+        // 45, 195, 95
+        groundImage.GetComponent<Renderer>().material.color = new Color(0.25f, 0.25f, 0.25f);
+        // 255, 255, 255
+        ohnishiSprite.GetComponent<Renderer>().material.color = new Color(0.25f, 0.25f, 0.25f);
+    }
+
+    void Update()
+    {
+        if (m_distanceTraveled < 13f)
+        {
+            Vector3 oldPosition = transform.position;
+            transform.Translate(.05f, 0, 0 * Time.deltaTime);
+            m_distanceTraveled += Vector3.Distance(oldPosition, transform.position);
+        }
+    }
+}
---
+++ JAPN350 Final/Assets/Scripts/MenuSceneChange.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuSceneChange : MonoBehaviour
+{
+    public Button startButton;
+    bool startFlash = false;
+    public Image flashImage;
+    public int flashTimes = 10;
+
+    void Start()
+    {
+        startButton.onClick.AddListener(TaskOnClickOne);
+    }
+
+    void TaskOnClickOne()
+    {
+        startFlash = true;
+        flashImage.gameObject.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (startFlash)
+        {
+
+            Color Opaque = new Color(1, 1, 1, 1);
+            flashImage.color = Color.Lerp(flashImage.color, Opaque, 6 * Time.deltaTime);
+            if (flashImage.color.a >= 0.8) //Scaling to Opaque
+            {
+                startFlash = false;
+            }
+        }
+        if (!startFlash)
+        {
+            Color Transparent = new Color(1, 1, 1, 0);
+            flashImage.color = Color.Lerp(flashImage.color, Transparent, 6 * Time.deltaTime);
+            if (flashImage.color.a <= 0.1) //Scaling back down to transparency
+            {
+                if (flashTimes == 0)
+                {
+                    SceneManager.LoadScene(sceneName: "BattleScene");
+                }
+                flashTimes = flashTimes - 1;
+                startFlash = true;
+            }
+        }
+    }
+}
